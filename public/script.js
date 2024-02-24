81c42bf9b0e7e7b8b619724e6b9b62ef12ac528e
akit <149289426+Sstudiosdev@users.noreply.github.com>
2024-02-24 11:21:46 -0600

    Update script.js

--- public/script.js
+++ public/script.js
@@ -14,20 +14,27 @@ window.onload = () => {
 
 // Formulario de subida de fotos
 const form = document.querySelector('form');
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', async (event) => {
     event.preventDefault();
     const formData = new FormData(form);
 
-    fetch('/upload', {
-        method: 'POST',
-        body: formData
-    })
-        .then(response => response.json())
-        .then(photo => {
-            const gallery = document.getElementById('gallery');
-            const img = document.createElement('img');
-            img.src = `/uploads/${photo.filename}`;
-            gallery.appendChild(img);
-        })
-        .catch(error => console.error('Error uploading photo:', error));
+    try {
+        const response = await fetch('/upload', {
+            method: 'POST',
+            body: formData
+        });
+        const data = await response.json();
+        if (response.status === 200) {
+            const uploadedImgContainer = document.getElementById('uploadedImgContainer');
+            const uploadedImg = document.createElement('img');
+            uploadedImg.src = `/uploads/${data.filename}`;
+            uploadedImg.className = 'uploaded-img';
+            uploadedImgContainer.appendChild(uploadedImg);
+        } else {
+            alert('Error uploading photo');
+        }
+    } catch (error) {
+        console.error('Error uploading photo:', error);
+    }
+
 });
