1dd06e6576c0c80f2afd68c180aac27e989b6c60
api-tsukasa <142162315+api-tsukasa@users.noreply.github.com>
2024-03-13 17:21:47 -0600

    Update dependencies.bat

--- start/dependencies.bat
+++ start/dependencies.bat
@@ -1,1 +1,14 @@
-npm i
+@echo off
+echo Installing dependencies...
+npm install
+if %errorlevel% neq 0 (
+    echo.
+    echo ERROR! There was a problem installing dependencies.
+    echo.
+    pause
+) else (
+    echo.
+    echo Dependencies installed successfully.
+    echo.
+    pause
+)
