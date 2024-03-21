a3245f10191cae5181c2ea47828c3fd9b5ba2852
[Bot Help] <164266627+photovoyagehelp@users.noreply.github.com>
2024-03-21 14:58:35 -0600

    Update captchaS.js

--- services/captchaS.js
+++ services/captchaS.js
@@ -1,3 +1,7 @@
+// PhotoVoyage
+// Code developed by the photoVoyage team and collaborators
+// https://github.com/api-tsukasa/PhotoVoyage/graphs/contributors
+
 function generateCaptcha() {
     const operators = ['+', '-', '*'];
     const num1 = Math.floor(Math.random() * 10);
